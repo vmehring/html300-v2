6f0e210ad76454e138ce629c7e506ec4e5e0d7cb
Valerie Mehring <vmehring@gmail.com>
2021-04-22 19:16:51 -0700

    lesson 3 in-class activity

--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,5 +1,16 @@
 // Create the Dog class constructor
-
+const dog = {
+  name: 'Marty',
+  age: 5,
+  breed: 'Corgi',
+  color: 'black and white',
+  barkLevel: 10,
+  energyLevel: 0,
+  pat() {
+    for barkLevel(i = 10; i > 0; i--;)
+    for energyLevel(i = 0; i < 10; i++;)
+  }
+}
 // Create Marty
 
 // Pat him 5 times
